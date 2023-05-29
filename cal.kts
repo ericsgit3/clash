f41ddae4e6a4479b99807f5725b213ea83a6e00b
ericsgit3 <134654778+ericsgit3@users.noreply.github.com>
2023-05-29 23:17:49 +0800

    Create cal.kts

---
+++ cal.kts
@@ -0,0 +1,31 @@
+import java.util.*
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    var num1: Double
+    var num2: Double
+    var operator: String
+
+    println("简单计算器程序")
+    println("---------------------")
+
+    print("输入第一个数字: ")
+    num1 = scanner.nextDouble()
+
+    print("输入第二个数字: ")
+    num2 = scanner.nextDouble()
+
+    print("选择操作符 (+, -, *, /): ")
+    operator = scanner.next()
+
+    val result = when (operator) {
+        "+" -> num1 + num2
+        "-" -> num1 - num2
+        "*" -> num1 * num2
+        "/" -> num1 / num2
+        else -> throw IllegalArgumentException("无效的操作符")
+    }
+
+    println("---------------------")
+    println("结果: $result")
+}
