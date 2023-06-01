8f2cac65afe032474781ad0290ef15e2fb5b4e8f
ericsgit3 <134654778+ericsgit3@users.noreply.github.com>
2023-06-01 20:05:11 +0800

    Update main.go

--- main/main.go
+++ main/main.go
@@ -6,7 +6,7 @@ import (
 func main() {
 	os.Args = getArgsV4Compatible()
 
-	base.RootCommand.Long = "Xray is a platform for building proxies."
+	base.RootCommand.Long = " a platform for building proxies."
 	base.RootCommand.Commands = append(
 		[]*base.Command{
 			cmdRun,
