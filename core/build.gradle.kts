d4d05b0b77b0efe77ccac3e0c6559670df5a48e7
ericsgit3 <134654778+ericsgit3@users.noreply.github.com>
2023-06-03 01:57:55 +0800

    Create build.gradle.kts

---
+++ core/build.gradle.kts
@@ -0,0 +1,12 @@
+import com.github.kr328.golang.GolangBuildTask
+import com.github.kr328.golang.GolangPlugin
+import java.io.FileOutputStream
+import java.net.URL
+import java.time.Duration
+
+plugins {
+    kotlin("android")
+    id("com.android.library")
+    id("kotlinx-serialization")
+    id("golang-android")
+}
