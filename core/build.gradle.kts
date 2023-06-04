1c05885ff41443e3d3faad03004d5d8bda1bb0c3
ericsgit3 <134654778+ericsgit3@users.noreply.github.com>
2023-06-04 13:07:00 +0800

    Update build.gradle.kts

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,7 +1,6 @@
 import com.github.kr328.golang.GolangBuildTask
 import com.github.kr328.golang.GolangPlugin
 import java.io.FileOutputStream
-import java.net.URL
 import java.time.Duration
 
 plugins {
